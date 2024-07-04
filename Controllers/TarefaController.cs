1ad05826716d982c9fc53aceb14acdd8e72e5e02
Miguel <miguel1411.mt@gmail.com>
2024-07-04 15:03:17 -0300

    Desafio concluidogit status

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -18,25 +18,26 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
         {
-            // TODO: Buscar o Id no banco utilizando o EF
-            // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
-            // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+            var tarefaId = _context.Tarefas.Find(id);
+
+            if (tarefaId == null)
+                return NotFound();
+
+            return Ok(tarefaId);
         }
 
         [HttpGet("ObterTodos")]
         public IActionResult ObterTodos()
         {
-            // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
+            var tarefas = _context.Tarefas;
+            return Ok(tarefas);
         }
 
         [HttpGet("ObterPorTitulo")]
         public IActionResult ObterPorTitulo(string titulo)
         {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o titulo recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
-            return Ok();
+            var tarefaTitle = _context.Tarefas.Where(tarefas => tarefas.Titulo.Contains(titulo));
+            return Ok(tarefaTitle);
         }
 
         [HttpGet("ObterPorData")]
@@ -49,8 +50,6 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("ObterPorStatus")]
         public IActionResult ObterPorStatus(EnumStatusTarefa status)
         {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o status recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
             var tarefa = _context.Tarefas.Where(x => x.Status == status);
             return Ok(tarefa);
         }
@@ -61,7 +60,8 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
-            // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
+            _context.Tarefas.Add(tarefa);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
 
@@ -73,12 +73,19 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefaBanco == null)
                 return NotFound();
 
-            if (tarefa.Data == DateTime.MinValue)
+            else if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
-            // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
-            // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
-            return Ok();
+            var tarefaId = _context.Tarefas.Find(id);
+
+            tarefaId.Titulo = tarefa.Titulo;
+            tarefaId.Descricao = tarefaBanco.Descricao;
+            tarefaId.Data = tarefa.Data;
+            tarefaId.Status = tarefa.Status;
+
+            _context.Tarefas.Update(tarefaId);
+            _context.SaveChanges();
+            return Ok(tarefaId);
         }
 
         [HttpDelete("{id}")]
@@ -89,7 +96,9 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefaBanco == null)
                 return NotFound();
 
-            // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
+            _context.Remove(tarefaBanco);
+            _context.SaveChanges();
+
             return NoContent();
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,23 +1,24 @@
-using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using TrilhaApiDesafio.Context;
+using TrilhaApiDesafio.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
+
+builder.Services.AddDbContext<OrganizadorContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
-builder.Services.AddControllers().AddJsonOptions(options =>
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -26,8 +27,81 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
-app.MapControllers();
+app.MapPost("CriarTarefa", (OrganizadorContext context, Tarefa tableTarefa) =>
+{
+    context.Tarefas.Add(tableTarefa);
+    context.SaveChanges();
+
+    return tableTarefa;
+});
+
+app.MapGet("ObterTarefa/{id}", (int id, OrganizadorContext context) => 
+{
+    var tarefaId = context.Tarefas.Find(id);
+
+    if (tarefaId == null)
+        Console.WriteLine("Tarefa não encontrada!");
+
+    return tarefaId;
+});
+
+app.MapPut("AtualizarTarefa/{id}", (int id, OrganizadorContext context, Tarefa tableTarefa) =>
+{
+    var tarefaId = context.Tarefas.Find(id);
+
+    if (tarefaId == null)
+        Console.WriteLine("Tarefa não encontrada!");
+
+    tarefaId.Titulo = tableTarefa.Titulo;
+    tarefaId.Descricao = tableTarefa.Descricao;
+    tarefaId.Data = tableTarefa.Data;
+    tarefaId.Status = tableTarefa.Status;
+
+    context.Tarefas.Update(tarefaId);
+    context.SaveChanges();
+
+    return tarefaId;
+});
+
+app.MapDelete("DeletarTarefa/{id}", (int id, OrganizadorContext context) =>
+{
+    var tarefaId = context.Tarefas.Find(id);
+
+    if (tarefaId == null)
+        Console.WriteLine("Tarefa não encontrada!");
+
+    context.Remove(tarefaId);
+    context.SaveChanges();
+
+    return $"Tarefa cujo id é {id} foi deletada com êxito!";
+});
+
+app.MapGet("ObterTodas", (OrganizadorContext context) =>
+{
+    var tarefas = context.Tarefas;
+    return tarefas;
+});
+
+app.MapGet("ObterPeloTitulo/{titulo}", (string titulo, OrganizadorContext context) =>
+{
+    var tarefaTitle = context.Tarefas.Where(tarefas => tarefas.Titulo.Contains(titulo));
+
+    return tarefaTitle;
+});
+
+app.MapGet("ObterPorData/{data}", (DateTime data, OrganizadorContext context) =>
+{
+    var tarefaDate = context.Tarefas.Where(x => x.Data.Date == data.Date);
+
+    return tarefaDate;
+});
+
+app.MapGet("ObterPorStatus/{status}", (EnumStatusTarefa status, OrganizadorContext context) =>
+{
+    var tarefaStatus = context.Tarefas.Where(tarefa => tarefa.Status == status);
+
+    return tarefaStatus;
+});
 
 app.Run();
